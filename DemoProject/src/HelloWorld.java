213aa04cb4729126d060b9614b54fe1113edf381
J. Drwenski <j.drwenski@medienberufe.de>
2021-11-24 09:45:55 +0100

    Zufallszahl ergänzt

--- DemoProject/src/HelloWorld.java
+++ DemoProject/src/HelloWorld.java
@@ -1,9 +1,14 @@
+import java.util.Random;
 
 public class HelloWorld {
 
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
 		System.out.println("How are you!");
+		
+		Random rnd = new Random();
+		int zufallszahl = rnd.nextInt(100);
+		System.out.println(zufallszahl);
 	}
 
 }
