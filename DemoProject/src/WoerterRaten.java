3094e30970817e76972d067378aca566f83a6533
J. Drwenski <j.drwenski@medienberufe.de>
2021-12-01 09:49:10 +0100

    Programmierübung a) Lösung.

--- DemoProject/src/WoerterRaten.java
+++ DemoProject/src/WoerterRaten.java
@@ -1,9 +1,23 @@
+import java.util.Random;
 
 public class WoerterRaten {
 
 	public static void main(String[] args) {
+		Random random = new Random();
+		
 		String wort = "BICYCLE";
+		char[] buchstaben = wort.toCharArray();
 
+		int zufall;
+		
+		for(int i = 0; i < wort.length(); i++) {
+			zufall = random.nextInt(wort.length());	
+			char tmp = buchstaben[i];
+			buchstaben[i] = buchstaben[zufall];
+			buchstaben[zufall] = tmp;
+		}
+		
+		System.out.println(new String(buchstaben));
 	}
 
 }
